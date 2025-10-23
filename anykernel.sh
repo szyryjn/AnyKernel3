930437f235c85557e36f70a6f27c75916d6883d1
szyryjn <amarkumarray060@gmail.com>
2025-10-23 08:18:29 +0000

    changed neolit to ksunext
    Signed-off-by: szyryjn <amarkumarray060@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neOliT for Realme Monet(RMX2020/RMX2027)
+kernel.string=KSN-NEXT for Realme Monet(RMX2020/RMX2027)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
