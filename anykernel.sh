634506eb410a972ad39200c1f029cc25f7c73cc0
szyryjn <amarkumarray060@gmail.com>
2026-03-21 10:13:08 +0530

    Updated Anykernel.sh for oneplus7t Support

--- anykernel.sh
+++ anykernel.sh
@@ -4,62 +4,131 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neOliT for Realme Monet(RMX2020/RMX2027)
+kernel.string=Satori
+kernel.compiler=AOSP Clang
+kernel.made=szyryjn
+message.word=Thank you for installing satori kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=rmx2020
-device.name2=RMX2020
-device.name3=RMX2027
-device.name4=RMX2191
-device.name5=RMX3171
-device.name6=RMX2193
-supported.versions=
-supported.patchlevels=
+device.name1=OnePlus7
+device.name2=guacamoleb
+device.name3=OnePlus7Pro
+device.name4=guacamole
+device.name5=OnePlus7ProTMO
+device.name6=guacamolet
+device.name7=OnePlus7T
+device.name8=hotdogb
+device.name9=OnePlus7TPro
+device.name10=hotdog
+device.name11=OnePlus7TProNR
+device.name12=hotdogg
+supported.versions=16
 '; } # end properties
 
 # shell variables
 block=/dev/block/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
-
+patch_vbmeta_flag=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Detect device and system
+if [ -e /system/etc/buildinfo/oem_build.prop ]; then
+  os="stock";
+  os_string="OxygenOS/HydrogenOS";
+else
+  os="custom";
+  os_string="a custom ROM";
+fi
+ui_print " " "You are on $os_string!";
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+if [ $os == "custom" ]; then
+  if [ -f $home/Image ]; then
+  mv $home/Image $home/Image-dtb;
+fi;
+  if [ -f $home/dtb ]; then
+    cp $home/*dtb .;
+  fi;
+fi;
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
+# Override DTB
+if [ $os == "stock" ]; then
+  mv $home/dtb $home/split_img/;
+fi
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# Move resetprop_static
+cp -rfp $home/resetprop_static /data/local/tmp/resetprop_static;
+chmod 755 /data/local/tmp/resetprop_static;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+if mountpoint -q /data; then
+  # Optimize F2FS extension list (@arter97)
+  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
+    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+    # Skip update if our list is already active
+    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
+      echo "Extension list up-to-date: $list_path"
+      continue
+    fi
 
-# end ramdisk changes
+    ui_print " " "Optimizing F2FS extension list..."
+    echo "Updating extension list: $list_path"
 
+    echo "Clearing extension list"
+
+    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
+    list_len="$(cat $list_path | wc -l)"
+    cold_count="$((list_len - hot_count))"
+
+    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
+    hot_list="$(tail -n$cold_count $list_path)"
+
+    for ext in $cold_list; do
+      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
+    done
+
+    for ext in $hot_list; do
+      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
+    done
+
+    echo "Writing new extension list"
+
+    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
+      [ ! -z $ext ] && echo "[c]$ext" > $list_path
+    done
+
+    for ext in $(cat $home/f2fs-hot.list); do
+      [ ! -z $ext ] && echo "[h]$ext" > $list_path
+    done
+  done
+fi
+
+# Install the boot image
 write_boot;
-## end install
+## end boot install
 
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
+
+## end install
